6d4e0df02bc97f9aac7df464f6e3a2d3f4a1fedb
Thomas <thomaschou861205@gmail.com>
2018-06-01 16:06:36 +0800

    add qsort_recursive

--- qsort4molecules.cpp
+++ qsort4molecules.cpp
@@ -1,82 +1,110 @@
 #include <iostream>
+#include <cstdio>
 #include <string>
 #include <stack>
+#include <vector>
+#include <iomanip>
 #include "element_table.h"
 
 using namespace std;
 
+typedef struct molecule {
+	string name;
+	string formula;
+	double mass;
+} Molecule;
+
 stack<double> Qmass;
 
 double getMoleculeMass(const string molecules, int &idx);
+double getMoleculeMass(const string molecules);
 inline int string2Int(string);
+void printArr(vector<Molecule *> arr, const int swap_time);
 
-int main(void){
-	int n;
-	string molecules;
+int main(void) {
+	int groups;
+	int molecules;
 
-	cin >> n;
-	while (n--){
-		int idx = 0;
+	cin >> groups;
+	cout << groups << endl;
+	for (int i = 0; i < groups; i++) {
+		int swap_time = 0;
+
 		cin >> molecules;
-		cout << getMoleculeMass(molecules, idx) << endl;
+		cout << molecules << endl;
+
+		vector<Molecule *> arr(molecules);
+		for (int j = 0; j < molecules; j++) {
+			Molecule *ptr = new Molecule;
+
+			cin >> ptr->name >> ptr->formula >> ptr->mass;
+			if (ptr->mass == 0) ptr->mass = getMoleculeMass(ptr->formula);
+			arr[j] = ptr;
+		}
+		printArr(arr, swap_time);
 	}
 	return 0;
 }
 
-double getMoleculeMass(const string molecules, int &idx){
+double getMoleculeMass(const string molecules){
+	int idx = 0;
+	return getMoleculeMass(molecules, idx);
+}
+
+double getMoleculeMass(const string molecules, int &idx) {
 	char ch;
 	double mass = 0;
 	string element;
 	map<string, double>::iterator iter;
 
 
-	while ((ch = molecules[idx]) != '\0'){
-		if (ch >= 'A' && ch <= 'Z'){
+	while ((ch = molecules[idx]) != '\0') {
+		if (ch >= 'A' && ch <= 'Z') {
 			element += ch;
 
-			if (!(molecules[idx + 1] >= 'a' && molecules[idx + 1] <= 'z')){
-				cout << element << ":";
+			if (!(molecules[idx + 1] >= 'a' && molecules[idx + 1] <= 'z')) {
+				// cout << element << ":";
 				iter = elementTable.find(element);
 
-				if (iter != elementTable.end()){
-					cout << iter->second << endl;
+				if (iter != elementTable.end()) {
+					// cout << iter->second << endl;
 
 					if (!(molecules[idx + 1] >= '0' && molecules[idx + 1] <= '9'))
 						mass += iter->second;
 					else Qmass.push(iter->second);
 
 				}
-				else cout <<"Not found" << endl;
+				// else cout <<"Not found" << endl;
 
 				element.clear();
 			}
 		}
-		else if (ch >= 'a' && ch <= 'z'){
+		else if (ch >= 'a' && ch <= 'z') {
 			element += ch;
-			cout << element << ":";
+			// cout << element << ":";
 			iter = elementTable.find(element);
 
-			if (iter != elementTable.end()){
-				cout << iter->second << endl;
+			if (iter != elementTable.end()) {
+				// cout << iter->second << endl;
 
 				if (!(molecules[idx + 1] >= '0' && molecules[idx + 1] <= '9'))
 					mass += iter->second;
 				else Qmass.push(iter->second);
-			}	
-			else cout <<"Not found" << endl;
+			}
+			// else cout <<"Not found" << endl;
 
 			element.clear();
 		}
-		else if (ch >= '0' && ch <= '9'){
+		else if (ch >= '0' && ch <= '9') {
 			element += ch;
 
-			if (!(molecules[idx + 1] >= '0' && molecules[idx + 1] <= '9')){
+			if (!(molecules[idx + 1] >= '0' && molecules[idx + 1] <= '9')) {
 				mass += string2Int(element) * Qmass.top();
 				Qmass.pop();
 				element.clear();
 			}
 		}
-		else if (ch == '(' || ch == '['){
+		else if (ch == '(' || ch == '[') {
 			idx++;
 			double tmp = getMoleculeMass(molecules, idx);
 
@@ -84,7 +112,7 @@ double getMoleculeMass(const string molecules, int &idx){
 				mass += tmp;
 			else Qmass.push(tmp);
 		}
-		else if (ch == ')' || ch == ']'){
+		else if (ch == ')' || ch == ']') {
 			return mass;
 		}
 		idx++;
@@ -93,13 +121,27 @@ double getMoleculeMass(const string molecules, int &idx){
 	return mass;
 }
 
-inline int string2Int(string str){
+inline int string2Int(string str) {
 	int ret = 0;
 
-	for (int i = 0; i < str.size(); i++){
+	for (int i = 0; i < str.size(); i++) {
 		ret *= 10;
 		ret += str[i] - '0';
 	}
 
 	return ret;
+}
+
+void printArr(vector<Molecule *> arr, const int swap_time){
+	for (int i = 0; i < arr.size(); i++){
+		(arr[i]->name).append(40 - (arr[i]->name).length(), ' ');
+		cout << arr[i]->name;
+
+		(arr[i]->formula).append(25 - (arr[i]->formula).length(), ' ');
+		cout << arr[i]->formula;
+
+		// cout << arr[i]->mass << endl;
+		printf("%10.2f\n", arr[i]->mass);
+	}
+	cout << swap_time << " swaps during qsort\n";
 }
--- sortQuick.cpp
+++ sortQuick.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
+#define N 16
 
 using namespace std;
 
+void swap(double &a, double &b){
+	double tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void qsort_recursive(double *arr, int left, int right){
+	if (left < right){
+		double pivot = arr[(left + right)/2];
+
+		int i = left;
+		int j = right;
+
+		while (i < j){
+			while (arr[i] < pivot) i++;
+			while (arr[j] > pivot) j--;
+			if (i < j) swap(arr[i], arr[j]);
+		}
+
+		qsort_recursive(arr, j + 1, right);
+		qsort_recursive(arr, left, i - 1);
+	}
+}
+
 int main(void){
+	double arr[N];
+	
+	for (int i = 0; i < N; i++) {
+		cin >> arr[i];
+		cout << arr[i] << " ";
+	}cout << endl;
+
+	// qsort_recursive(arr, 0, N - 1);
+
+	for (int i = 0; i < N; i++) cout << arr[i] << " ";
+	cout << endl;
+
 	return 0;
 }
